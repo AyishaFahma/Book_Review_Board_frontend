fb59bd981d215358b9251faaf9ee1d752c6242a5
AyishaFahma <mailtofahma@gmail.com>
2025-08-14 00:51:24 +0530

    serverurl updated

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,5 @@
 
 
-export const serverUrl = 'http://localhost:4000'
+//export const serverUrl = 'http://localhost:4000'
+
+export const serverUrl =  'https://book-review-board-backend.onrender.com'
